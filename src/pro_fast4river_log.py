1f1ad6436be6f64439171745071b1de1d47ddc5a
iyt1823 <yourtellor@gmail.com>
2025-04-12 14:46:11 +0900

    Update development log at 2025-04-12 14:46:11

--- src/pro_fast4river_log.py
+++ src/pro_fast4river_log.py
@@ -126,3 +126,18 @@ class Observer:
     def notify(self):
         for observer in self._observers:
             observer.update()
+
+# Update at 2025-04-12 14:46:11
+# Added unit tests
+
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
