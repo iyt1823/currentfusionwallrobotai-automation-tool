533ecb7a47123e387262b69f6de4be579c6a28c7
iyt1823 <yourtellor@gmail.com>
2025-04-29 01:25:10 +0900

    Update development log at 2025-04-29 01:25:08

--- src/pro_fast4river_log.py
+++ src/pro_fast4river_log.py
@@ -416,3 +416,24 @@ def get_date_range(start_date, end_date):
         date_list.append(current_date)
         current_date += timedelta(days=1)
     return date_list
+
+# Update at 2025-04-29 01:25:08
+# Improved performance
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
