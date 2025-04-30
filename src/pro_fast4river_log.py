ff1bfdba7ad63cce98d64feb9575751b7bd3e2f2
iyt1823 <yourtellor@gmail.com>
2025-04-30 12:49:08 +0900

    Update development log at 2025-04-30 12:49:07

--- src/pro_fast4river_log.py
+++ src/pro_fast4river_log.py
@@ -437,3 +437,10 @@ class LinkedList:
         while current.next:
             current = current.next
         current.next = new_node
+
+# Update at 2025-04-30 12:49:07
+# Added unit tests
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n-1)
